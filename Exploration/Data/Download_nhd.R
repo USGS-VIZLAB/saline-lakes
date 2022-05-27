7082d2d09e5e6137a03ce15ff87a03138f5e382c
Margaux Sleckman <msleckman@usgs.gov>
2022-05-27 12:53:39 -0700

    adding exploratory markdown + source file for downloading NHD high res

---
+++ Exploration/Data/Download_nhd.R
@@ -0,0 +1,31 @@
+library(sf)
+library(nhdplusTools)
+library(dplyr)
+library(stringr)
+
+## CRS: keeping crs at 4326 for now 
+selected_crs = 4326
+
+## list of lakes
+lakes_excel <- readxl::read_xlsx('Data/Lakes_list.xlsx', col_types = 'text')
+
+## Turning lakes into SF obj
+lakes <- lakes_excel %>% 
+  ## note - lon comes first in st_as_sf() 
+  st_as_sf(coords = c('Lon','Lat'), crs = 4326) %>% 
+  rename(Point_geometry = geometry, lake = `Lake Ecosystem`) %>% 
+  mutate(lake = str_to_title(lake)) %>% 
+  mutate(lake_name_shrt = trimws(str_replace(lake, pattern = 'Lake', replacement = "")))
+
+## Define temp dir in Data folder
+nhdhr_dir <- file.path('Data', "nhdhr_lakes")
+
+## Get huc08 for focal lakes
+huc8 <- get_huc8(AOI = lakes$Point_geometry) %>% unique()
+
+## huc8 for download
+huc8_cmids_for_download <- substr(huc8$huc8, start = 1, stop = 4) %>% unique()
+
+## nhdplustools 
+nhdplusTools::download_nhdplushr(nhdhr_dir, huc8_cmids_for_download)
+
