7e886e14cfc7ef355fe52e0e8fa71ebd7cc115bd
Margaux Sleckman <msleckman@usgs.gov>
2022-06-01 13:36:00 -0700

    final additions

--- Exploration/Data/Download_nhd.R
+++ Exploration/Data/Download_nhd.R
@@ -3,28 +3,24 @@ library(nhdplusTools)
 library(dplyr)
 library(stringr)
 
-## CRS: keeping crs at 4326 for now 
-selected_crs = 4326
+download_nhdhr <- function(lakes_df, point_geom_col = 'Point_geometry', output_file_path){
+  #' @description 
+  #' @param lakes_df lakes_dataset with point coords of selected lakes
+  #' @param point_geom_col char. Name of point geom column in lakes_df
+  #' @param output_file_path out filepath for download data. Download process will create subfolders  for huc areas
+  
+  ## Define temp dir in Data folder
+  nhdhr_dir <- file_path
 
-## list of lakes
-lakes_excel <- readxl::read_xlsx('Data/Lakes_list.xlsx', col_types = 'text')
+  ## Get huc08 for focal lakes and extract comids
+  huc8 <- get_huc8(AOI = lakes_df[point_geom_col])
+  
+  huc8_comids <- substr(huc8$huc8, start = 1, stop = 4) %>% unique()
 
-
-## Turning lakes into SF obj
-lakes <- lakes_excel %>% 
-  ## note - lon comes first in st_as_sf() 
-  st_as_sf(coords = c('Lon','Lat'), crs = 4326) %>% 
-  rename(Point_geometry = geometry, lake = `Lake Ecosystem`) %>% 
-  mutate(lake = str_to_title(lake)) %>% 
-  mutate(lake_name_shrt = trimws(str_replace(lake, pattern = 'Lake', replacement = "")))
-
-## Define temp dir in Data folder
-nhdhr_dir <- file.path('Data', "nhdhr_lakes")
-
-## Get huc08 for focal lakes and extract comids
-huc8 <- get_huc8(AOI = lakes$Point_geometry)
-huc8_comids <- substr(huc8$huc8, start = 1, stop = 4) %>% unique()
-
-## nhdplustools
-nhdplusTools::download_nhdplushr(nhdhr_dir, huc8_comids)
+  ## nhdplustools
+  nhdplusTools::download_nhdplushr(nhdhr_dir, huc8_comids)
+  
+  return(list.files(nhdhr_dir))
+  
+}
 
