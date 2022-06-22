34b55beed7f625ec691191debb7a05ae88e9e048
Margaux Sleckman <msleckman@usgs.gov>
2022-06-22 12:51:19 -0700

    adding path to backup folder

--- _targets.R
+++ _targets.R
@@ -1,6 +1,6 @@
 library(targets)
 
-options(tidyverse.quiet = TRUE)
+options(tidyverse.quiet = TRUE, timeout = 300)
 tar_option_set(packages = c("tidyverse","sbtools","sf","nhdplusTools",'dplyr','readxl','readr','stringr'))
 
 source("1_fetch.R")
@@ -11,7 +11,10 @@ dir.create('1_fetch/in/nhdhr', showWarnings = FALSE)
 dir.create('1_fetch/in/states_shp', showWarnings = FALSE)
 
 ## CRS: keeping crs at 4326 for now
-selected_crs = 4326
+selected_crs <-  4326
+
+## back up path to the high res nhd data needed for querying lakes and flowlines
+p1_download_nhdhr_lakes_backup_download_path <- '1_fetch/in/nhdhr_backup'
 
 # Return the complete list of targets
 c(p1_targets_list)
