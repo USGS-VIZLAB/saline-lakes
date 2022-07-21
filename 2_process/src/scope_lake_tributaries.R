1563a2a26f3914bae22cbda128368be44ea143b8
Margaux Sleckman <msleckman@usgs.gov>
2022-07-21 10:39:09 -0700

    change buffer val

--- 2_process/src/scope_lake_tributaries.R
+++ 2_process/src/scope_lake_tributaries.R
@@ -1,14 +1,21 @@
 scope_lake_tributaries <- function(fline_network,
                                  lakes_sf,
-                                 buffer_dist = 1000000,
+                                 buffer_dist = 10000,
                                  realization = c('flowline','catchment'),
                                  stream_order = 3){
 
+    fline_network = p1_lake_flowlines_huc8_sf
+    lakes_sf = p2_saline_lakes_sf
+    buffer_dist = 10000
+    realization = c('flowline','catchment')
+    stream_order = 3
+  
   # CHECKS
   ## flines layer geometries 
   if(any(!st_is_valid(fline_network))){
     fline_network <- st_make_valid(fline_network)
-    message(paste0(fline_network, 'shp geometries fixed'))}
+    message(paste0(fline_network, 'shp geometries fixed'))
+    }
   
   ## lake layer geometries 
   if(any(!st_is_valid(lakes_sf))){
@@ -17,15 +24,15 @@ scope_lake_tributaries <- function(fline_network,
   }
   
   ## Match crs
-  if(!st_crs(fline_network) == st_crs(lakes_sf)){
+  if (!st_crs(fline_network) == st_crs(lakes_sf)) {
     message('crs are different. Transforming ...')
-    fline_network <- st_transform(fline_network, crs = st_crs(lakes_sf))}
+    fline_network <- st_transform(fline_network, crs = st_crs(lakes_sf))
     if(st_crs(fline_network) == st_crs(lakes_sf)){
-      message('crs now aligned')}
-    else{
+       message('crs now aligned')}
+  } else {
     message('crs are already aligned')
   }
-  
+    
   # Comid reaches in lake = buffering by 10000 because many lakes don't have flowlines that go right into it
   ## NOTE - Chose to buffer to be able to get all incoming tribs (issues arrive around franklin lake and carson lake)
   lakes_buffered_sf <- lakes_sf %>% st_buffer(dist= buffer_dist)
@@ -37,7 +44,7 @@ scope_lake_tributaries <- function(fline_network,
   lake_UT <- get_UT(comid = reach_in_lake$comid, network = fline_network)
   
   # fetch identified upstream tributaries
-  final_lake_tributaries <- get_nhdplus(comid = lake_UT, realization = realization, streamorder = stream_order)
+  final_lake_tributaries <- get_nhdplus(comid = lake_UT, realization = 'flowline', streamorder = stream_order)
   
   return(final_lake_tributaries)
   
