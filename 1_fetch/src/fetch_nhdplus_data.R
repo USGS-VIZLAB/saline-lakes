5fe4b9221380c0d44f7a29a01837cccd7a0b3b04
Margaux Sleckman <36547359+msleckman@users.noreply.github.com>
2022-08-16 10:43:10 -0700

    Update 1_fetch/src/fetch_nhdplus_data.R
    Co-authored-by: Cee Nell <cnell@usgs.gov>

--- 1_fetch/src/fetch_nhdplus_data.R
+++ 1_fetch/src/fetch_nhdplus_data.R
@@ -4,7 +4,7 @@
 get_downloaded_nhd_data <- function(gdb_path, out_gpkg_path, layer){
   #' @description wrapper function for get_nhdplushr
   #' @param gbd_path downloaded gdb from nhdplushr (this should be the output of nhdplusTools::download_nhdplushr())
-  #' @param out_gpkg_path outpath for the gpkg in which the differen layers of nhdplushr data will be stored
+  #' @param out_gpkg_path outpath for the gpkg in which the different layers of nhdplushr data will be stored
   #' @param layer layer or vector of layers. see details for layer param in nhdplusTools::get_nhdplushr()
     
   get_nhdplushr(hr_dir = gdb_path,
