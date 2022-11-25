d8d95f3c86abff75044ba466639dce6f582b800c
Margaux Sleckman <msleckman@usgs.gov>
2022-11-25 15:15:45 -0800

    saving to RDS now

--- 2_process_sw_gw_site_data.R
+++ 2_process_sw_gw_site_data.R
@@ -5,39 +5,38 @@ p2_sw_gw_site_targets_list <- list(
   ## simplified vrn of all site in watersheds(regardless of whether there is relevant data at that site) + all site types
   tar_target(p2_site_in_watersheds_sf,
              p1_site_in_watersheds_sf %>% 
-               select(agency_cd, site_tp_cd, site_no, Name, geometry)
+               select(site_tp_cd, site_no, Name, geometry)
              ),
   
-  
-  
   ## get just gw sites with outputed data for 2000-2020 (timeframe to change)
-  tar_target(p2_nwis_dv_gw_data_sf, 
+  tar_target(p2_nwis_dv_gw_data, 
              p1_nwis_dv_gw_data %>%
                left_join(p2_site_in_watersheds_sf, by = 'site_no') %>%
-               filter(grepl('GW',site_tp_cd)) %>% 
                st_as_sf() %>% 
-               mutate(lon = st_coordinates(.)[,1], lat = st_coordinates(.)[,2])
+               mutate(lon = st_coordinates(.)[,1], lat = st_coordinates(.)[,2]) %>% 
+               filter(grepl('GW',site_tp_cd)) %>% 
+               st_drop_geometry() %>% 
+               ## quickly re-organizing cols
+               select(!starts_with('X_'),starts_with('X_'))
              ),
   
   ## getting all sites along lake 
   tar_target(p2_sw_streamorder3_sites,
              sites_along_waterbody(p2_site_in_watersheds_sf,
                                    p2_lake_tributaries,
-                                   lake_waterbody = FALSE,
-                                   buffer_m = 1000)
+                                   lake_waterbody = FALSE)
   ),
   
   ## this takes a 5+ minutes due to time for buffer of tributaries to generate
   tar_target(p2_sw_in_lake_sites,
              sites_along_waterbody(p2_site_in_watersheds_sf,
                                    p2_saline_lakes_sf,
-                                   lake_waterbody = TRUE,
-                                   buffer_m = 1000)
+                                   lake_waterbody = TRUE)
              
   ),
   
   ## get just sw sites with outputed data for 2000-2020 (timeframe to change) with stream order category column
-  tar_target(p2_nwis_dv_sw_data_sf, 
+  tar_target(p2_nwis_dv_sw_data, 
              p1_nwis_dv_sw_data %>%
                left_join(p2_site_in_watersheds_sf, by = 'site_no') %>%
                filter(site_tp_cd %in% c('LK','WE') | grepl('ST',site_tp_cd)) %>% 
@@ -47,7 +46,11 @@ p2_sw_gw_site_targets_list <- list(
                  TRUE ~ 'not along SO 3+'
                  )) %>% 
                st_as_sf() %>% 
-               mutate(lon = st_coordinates(.)[,1], lat = st_coordinates(.)[,2])
+               mutate(lon = st_coordinates(.)[,1], lat = st_coordinates(.)[,2]) %>% 
+               st_drop_geometry() %>% 
+               ## quickly re-organizing cols
+               select(!starts_with('X_'),starts_with('X_'))
+             
   )
   
 
